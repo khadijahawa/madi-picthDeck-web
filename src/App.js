fd0770c20fdb78f9222e33dcfcf32d34e0f46629
Khadija Hawa <khadijahawa555@gmail.com>
2023-01-30 18:03:07 +0300

    testing loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Care from "./pages/Care";
 import Repair from "./pages/Repair";
@@ -8,35 +7,15 @@ import OriginStory from "./pages/OriginStory";
 import Resale from "./pages/Resale";
 
 const App = () => {
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => setScreenWidth(window.innerWidth);
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
-    <div className={`App ${screenWidth < 600 ? "mobile" : "desktop"}`}>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/care">
-          <Care />
-        </Route>
-        <Route path="/repair">
-          <Repair />
-        </Route>
-        <Route path="/origin-story">
-          <OriginStory />
-        </Route>
-        <Route path="/resale">
-          <Resale />
-        </Route>
-      </Switch>
+    <div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/care" element={<Care />} />
+        <Route path="/repair" element={<Repair />} />
+        <Route path="/origin-story" element={<OriginStory />} />
+        <Route path="/resale" element={<Resale />} />
+      </Routes>
     </div>
   );
 };
